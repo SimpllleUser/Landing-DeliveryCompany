4b4b398c724d34812fe1caebcd07321afc594f57
SimpllleUser <raybek11@gmail.com>
2021-02-28 11:25:36 +0300

    ADDED slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("Work main js")
+
+// ===== INTI SLIDER
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#service-slider' ).mount();
+} );
+
+// ===== / INTI SLIDER
