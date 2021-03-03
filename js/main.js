e7f71d9c0904b3444e805217f08cedd61dee7500
SimpllleUser <raybek11@gmail.com>
2021-03-03 10:47:50 +0300

    Add animation to bg bergore body

--- js/main.js
+++ js/main.js
@@ -2,43 +2,42 @@ console.log("Work main js")
 
 // ===== INTI SLIDER
 
-document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '#service-slider',
-    {
-        type:"loop"
-    } )
-    .mount();
-} );
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#service-slider',
+        {
+            type: "loop"
+        })
+        .mount();
+});
 
 // ===== / INTI SLIDER
 
-const showMobileMenu =  async () => {
+const showMobileMenu = async () => {
     const navBar = document.getElementsByClassName('navbar-nav')[0];
     navBar.style.display = 'block';
-    navBar.setAttribute('class', navBar.getAttribute('class') +  ' mobile-navb-bar')
+    navBar.setAttribute('class', navBar.getAttribute('class') + ' mobile-navb-bar')
     setTimeout(() => {
         navBar.style.opacity = '1';
-    },100)
-    body[0].style.overflow = 'hidden'
+    }, 100)
+    body.setAttribute('data', 'modal-show')
 }
 
-const hideMobileMenu =  () => {
+const hideMobileMenu = () => {
     const navBar = document.getElementsByClassName('navbar-nav')[0];
     navBar.style.opacity = '0';
 
     setTimeout(() => {
-        navBar.setAttribute('class', navBar.getAttribute('class').replace('mobile-navb-bar',''))
-        body[0].style.overflow = 'auto'
+        navBar.setAttribute('class', navBar.getAttribute('class').replace('mobile-navb-bar', ''))
+        body.setAttribute('data', '')
         navBar.style.display = 'none';
-    },300)
+    }, 300)
 
 }
 
 
-const burgerBtn = document.getElementsByClassName('burger-btn')
-const hideNavBar = document.getElementsByClassName('hide-nav-bar')
-const body = document.getElementsByTagName('body')
+const burgerBtn = document.getElementsByClassName('burger-btn')[0]
+const hideNavBar = document.getElementsByClassName('hide-nav-bar')[0]
+const body = document.getElementsByTagName('body')[0]
 
-burgerBtn[0].addEventListener('click', showMobileMenu)
-hideNavBar[0].addEventListener('click', hideMobileMenu)
-
+burgerBtn.addEventListener('click', showMobileMenu)
+hideNavBar.addEventListener('click', hideMobileMenu)
